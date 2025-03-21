aedc96d74a25eefc43416ff3611cb4ae983df9f5
KellyR <kellyruiz.dev@gmail.com>
2025-03-20 22:33:36 -0700

    initial project

---
+++ app/src/main/java/com/example/cryptocompose/common/Constants.kt
@@ -0,0 +1,4 @@
+package com.example.cryptocompose.common
+
+object Constants {
+}
---
+++ app/src/main/java/com/example/cryptocompose/data/remote/CoinPaprikaAPI.kt
@@ -0,0 +1,6 @@
+package com.example.cryptocompose.data.remote
+
+interface CoinPaprikaAPI {
+
+    //@GET("/v1/coins")
+}
